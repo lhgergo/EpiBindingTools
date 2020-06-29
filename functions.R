62c71d756e3a110ccaa242aa9ac830d96bf94b1d
lhgergo <lefthandgergo@gmail.com>
2020-06-30 00:23:26 +0200

    added fresh CreateRecognMatrix function, using bash command "tr" to simplify result files during loading

--- functions.R
+++ functions.R
@@ -2,6 +2,7 @@ library(doParallel)
 library(foreach)
 
 # RunNetMHCpan ----------
+# A user-friendly launcher for single-thread/parallel netMHCpan runs.
 # __inputs__
 # alleles_loc: path to a file containing alleles to predict binding of epitopes to. Allele names in netMHCpan-compatible format.
 # pep_loc: path to .pep file, containing one epitope per line.
@@ -31,4 +32,34 @@ RunNetMHCpan <- function(alleles_loc, pep_loc, results_out_loc, parallel_threads
     foreach (i = 1:length(strings)) %dopar% {system(strings[i])}
     stopCluster(c3)
   }
+}
+
+# CreateRecognMatrix
+# Loads and processes netMHCpan 4.0 result files into recogn matrices
+# __inputs__
+# results_out_loc: path to directory containing netMHCpan result files.
+# __outputs__
+# A list, containing two matrices (one for aff and one for rp data).
+# In each matrix the rows represent alleles, the cols represent epitopes, and the numerics in the cells represent binding values
+
+CreateRecognMatrix <- function(results_out_loc) {
+  # collecting result file paths
+  resfiles_paths <- list.files(results_out_loc, full.names = T)
+  
+  # loading result files into a simplified list
+  pblapply(resfiles_paths, function(current_path) {
+    system(paste0("tail -n +51 ", current_path,  " | head -n -5 | tr -s ' '"), intern = T) %>% 
+      strsplit(" ") %>% sapply(function(x) {x[c(3, 4, 14, 15)]}) %>% t()
+  }) -> resdata
+  
+  # collecting alleles and epitopes
+  alleles <- sapply(resdata, function(x) {x[1, 1]})
+  epitopes <- resdata[[1]][, 2]
+  
+  # transforming everything into matrix format
+  recogn_matrix_aff <- t(sapply(resdata, function(x) {as.numeric(x[, 3])})) %>% set_rownames(alleles) %>% set_colnames(epitopes)
+  recogn_matrix_rp <- t(sapply(resdata, function(x) {as.numeric(x[, 4])})) %>% set_rownames(alleles) %>% set_colnames(epitopes)
+  
+  # returning matrices
+  list(aff = recogn_matrix_aff, rp = recogn_matrix_rp)
 }
